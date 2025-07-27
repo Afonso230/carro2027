ef0fdf22c4487d97104ef83c134e3d2454d91789
manueltomas <manuel.tomas1@outlook.com>
2025-07-27 01:09:57 +0100

    Restyling navbar for mobile devices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'carro27';
+
+  opened = false;
+
+  triggerNavbar() {
+    this.opened = !this.opened;
+  }
 }
