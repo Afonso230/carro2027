0aaa1725b47cf8c689f27c1fa0a2aa69ef07c537
manueltomas <manuel.tomas1@outlook.com>
2025-08-28 01:20:16 +0100

    Implement calculate paid/unpaid quotas for users

--- src/app/quotas.service.ts
+++ src/app/quotas.service.ts
@@ -22,9 +22,81 @@ export class QuotasService {
     return this.storageService.getData(`quotas/${month}`)
   }
 
+  isUserInPayments(userId, payments){
+    for(var userIdQuotas in payments){
+      if(userIdQuotas === userId){
+        return payments[userIdQuotas].data;
+      }
+    }
+    return null;
+  }
+
   getUnpaidQuotasForUser(user){
-    return this.storageService.getData(`quotas`).pipe(map(result=>{
-      return result
+    return this.storageService.getData(`quotas`).pipe(map(quotas=>{
+      console.log(quotas);
+      var calculatedQuotas = [];
+      for(var month in quotas){
+        var monthPayments = quotas[month]["pagamentos"];
+        if(!this.isUserInPayments(user, monthPayments)){
+          calculatedQuotas.push({
+            month: month,
+            valor: quotas[month]["valor"]
+          })
+        }
+      }
+      calculatedQuotas.sort((quota1, quota2) => {
+        var month1 = this.convertThreeCharacterMonthIntoIndex(quota1.month.substring(0,3));
+        var year1 = quota1.month.substring(3);
+        var month2 = this.convertThreeCharacterMonthIntoIndex(quota2.month.substring(0,3));
+        var year2 = quota2.month.substring(3);
+        if(year1 < year2){
+          return -1;
+        }else if(year1 > year2){
+          return 1;
+        }else if(month1 < month2){
+          return -1;
+        }else if(month2 > month1){
+          return 1;
+        }else{
+          return 0;
+        }
+      })
+      return calculatedQuotas
+    })) 
+  }
+
+  getPaidQuotasForUser(user){
+    return this.storageService.getData(`quotas`).pipe(map(quotas=>{
+      var calculatedQuotas = [];
+      for(var month in quotas){
+        var monthPayments = quotas[month]["pagamentos"];
+        var paymentDate = this.isUserInPayments(user, monthPayments)
+        if(paymentDate){
+          calculatedQuotas.push({
+            month: month,
+            valor: quotas[month]["valor"],
+            paymentDate: new Date(paymentDate)
+          })
+        }
+      }
+      calculatedQuotas.sort((quota1, quota2) => {
+        var month1 = this.convertThreeCharacterMonthIntoIndex(quota1.month.substring(0,3));
+        var year1 = quota1.month.substring(3);
+        var month2 = this.convertThreeCharacterMonthIntoIndex(quota2.month.substring(0,3));
+        var year2 = quota2.month.substring(3);
+        if(year1 < year2){
+          return -1;
+        }else if(year1 > year2){
+          return 1;
+        }else if(month1 < month2){
+          return -1;
+        }else if(month2 > month1){
+          return 1;
+        }else{
+          return 0;
+        }
+      })
+      return calculatedQuotas
     })) 
   }
 
--- src/app/see-account-dialog/see-account-dialog.component.ts
+++ src/app/see-account-dialog/see-account-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { QuotasService } from '../quotas.service';
 
 
 export interface UserData {
@@ -20,9 +21,18 @@ export class SeeAccountDialogComponent {
  constructor(
   public dialogRef : MatDialogRef<SeeAccountDialogComponent>,
   @Inject(MAT_DIALOG_DATA) public data : UserData,
-  
+  private quotasService : QuotasService
  ){
   console.log(data.name)
+ }
+
+ ngOnInit(){
+  this.quotasService.getUnpaidQuotasForUser(this.data.id).subscribe((value) => {
+    console.log(value);
+  })
+  this.quotasService.getPaidQuotasForUser(this.data.id).subscribe((value) => {
+    console.log(value);
+  })
  }
 
  fecharDialogo(): void {
