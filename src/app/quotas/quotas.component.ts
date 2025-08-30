07e30ea777a453d39b465b89e30f14b9adbfb463
Fonsi <afonsosvtomas@gmail.com>
2025-08-30 02:07:40 +0100

    Quotas ts started implementation

--- src/app/quotas/quotas.component.ts
+++ src/app/quotas/quotas.component.ts
@@ -1,4 +1,26 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
+import { QuotasService } from '../quotas.service';
+import { MatTableDataSource } from '@angular/material/table';
+
+
+export interface UserData {
+  id : string,
+  name : string,
+  type : number
+}
+
+export interface PaidQuotas {
+  month : string,
+  paymentDay : Date
+  valor : number,
+  
+}
+
+export interface UnpaidQuota {
+  month: string;
+  valor: number;
+}
 
 @Component({
     selector: 'app-quotas',
@@ -7,5 +29,69 @@ import { Component } from '@angular/core';
     standalone: false
 })
 export class QuotasComponent {
+
+  userId = "jaP0FMo7EmNgEHBp0zub1CBqtn52"
+  paidQuotas: PaidQuotas[] = [];
+  unpaidQuotas : UnpaidQuota[] = [];
+
+  dataSourcePaid ;
+  dataSourceUnpaid ;
+
+  displayedColumnsPaid = ['month','valor','paymentDate']
+
+ constructor(
+  private quotasService : QuotasService,
+  private breakPointObserver : BreakpointObserver
+ ){
+ }
+
+ ngOnInit(){
+  this.breakPointObserver.observe([Breakpoints.Small,Breakpoints.Handset]).subscribe((result)=>{
+      if (result.matches){
+        this.displayedColumnsPaid = ['month','paymentDate']
+      } else {
+        this.displayedColumnsPaid = ['month','valor','paymentDate']
+      }
+  })
+
+  this.quotasService.getUnpaidQuotasForUser(this.userId).subscribe((value) => {
+    for(var quota of value){
+      var multa = this.quotasService.calculateFineForDate(new Date(), quota.month)
+      var valorTotal = quota.valor + multa
+      quota.valor = valorTotal
+      quota.month = this.getMonthStringFromMonthCode(quota.month)
+    }
+    this.unpaidQuotas = value
+    this.dataSourceUnpaid = new MatTableDataSource(this.unpaidQuotas);
+    console.log(this.unpaidQuotas, "unpaidQuotas");
+  })
+
+  this.quotasService.getPaidQuotasForUser(this.userId).subscribe((value) => {
+    for(var quota of value){
+      var multa = this.quotasService.calculateFineForDate(quota.paymentDate, quota.month)
+      var valorTotal = quota.valor + multa
+      quota.valor = valorTotal
+      quota.month = this.getMonthStringFromMonthCode(quota.month)
+    }
+    this.paidQuotas = value
+    this.dataSourcePaid = new MatTableDataSource(this.paidQuotas);
+    console.log(this.paidQuotas, "paidQuotas");
+  })
+}
+
+  getMonthStringFromMonthCode(monthCode){
+    // month code = out2025
+    var ano = parseInt(monthCode.substring(3))
+    // ano = 2025
+    var monthIndex = this.quotasService.convertThreeCharacterMonthIntoIndex(monthCode.substring(0,3))
+    // monthIndex = 9
+    var dataAescrever = new Date(ano, monthIndex , 1);
+    //dataAescrever = 1 de outubro de 2025
+    var stringMes = dataAescrever.toLocaleDateString('pt-PT', { month: 'long', year: 'numeric' });
+    //atringMes = outubro de 2025
+    var stringMesApresentada = stringMes.charAt(0).toUpperCase() + stringMes.slice(1);
+    // Outubro de 2025
+    return stringMesApresentada 
+  }
 
 }
